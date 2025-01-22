8e8c29a15b0a73ad194956b930706afa69aaf24f
Rahat Bano <160172836+RahatBano58@users.noreply.github.com>
2025-01-22 06:30:40 +0500

    Update client.ts

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -4,7 +4,7 @@ import { apiVersion, dataset, projectId } from '../env'
 export const client = createClient({
   projectId:"lfjj7qeb",
   dataset:"production,
-  apiVersion:"2-25-01-21,
+  apiVersion:"2025-01-21,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
   token: "skyrfu3q1waMhKmkclJjXgxODyl8jizM1Ygvx8gct28ZBaG5OfGRUdgVfDsfcK7LpWJI7oj26dXJ7e1kJ7SWnnXDlbUE9uOqXGyQ8iAzztVZvSJN7T6OcOGcQsWgGKAPyXcM05ihr3x6nct85JBNbDlVtHLvcp9HZhk7XUewCyb4qy0JSWAY",
 })
